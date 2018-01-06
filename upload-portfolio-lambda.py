a84655b7c7a8bd160eaa05e2f75e0b25ef2636c3
Nicolas Spencer <nicolaspencer@gmail.com>
2018-01-05 23:58:38 -0300

    update del archivo upload-portfolio-lambda.py para agregar lo asociado con SNS

--- upload-portfolio-lambda.py
+++ upload-portfolio-lambda.py
@@ -5,20 +5,29 @@ import zipfile
 import mimetypes
 
 def lambda_handler(event, context):
-    s3 = boto3.resource('s3', config=Config(signature_version='s3v4'))
 
-    portfolio_bucket = s3.Bucket('portfolio.nicolaspencer.net')
-    build_bucket = s3.Bucket('portfoliobuild.nicolaspencer.net')
+    sns = boto3.resource('sns')
+    topic = sns.Topic('arn:aws:sns:us-east-1:812761597716:DeployPortfolioTopic')
 
-    portfolio_zip = StringIO.StringIO()
-    build_bucket.download_fileobj('portfoliobuild.zip', portfolio_zip)
+    try:
+        s3 = boto3.resource('s3', config=Config(signature_version='s3v4'))
 
-    with zipfile.ZipFile(portfolio_zip) as myzip:
-        for nm in myzip.namelist():
-            obj = myzip.open(nm)
-            portfolio_bucket.upload_fileobj(obj, nm,
-              ExtraArgs={'ContentType': mimetypes.guess_type(nm)[0]})
-            portfolio_bucket.Object(nm).Acl().put(ACL='public-read')
+        portfolio_bucket = s3.Bucket('portfolio.nicolaspencer.net')
+        build_bucket = s3.Bucket('portfoliobuild.nicolaspencer.net')
 
-    print 'Job Done!'
-    return 'Hello from Lambda'
+        portfolio_zip = StringIO.StringIO()
+        build_bucket.download_fileobj('portfoliobuild.zip', portfolio_zip)
+
+        with zipfile.ZipFile(portfolio_zip) as myzip:
+            for nm in myzip.namelist():
+                obj = myzip.open(nm)
+                portfolio_bucket.upload_fileobj(obj, nm,
+                  ExtraArgs={'ContentType': mimetypes.guess_type(nm)[0]})
+                portfolio_bucket.Object(nm).Acl().put(ACL='public-read')
+
+        print 'Job Done!'
+        topic.publish(Subject="Portfolio Deployed", Message="El porttafolio fue desplegado exitosamente!")
+
+    except:
+        topic.publish(Subject="Portfolio Failed To Deploy", Message="Ocurrio un error en el despliegue :(")
+        raise
