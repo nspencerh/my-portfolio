aea81164dd9904391d82a68f725007e9f81725f5
Nicolas Spencer <nicolaspencer@gmail.com>
2018-01-10 01:28:37 -0300

    se modifica index.html, se saca el codigo  y se lleva a example-work y main utilizando React

--- js/main.js
+++ js/main.js
@@ -1,5 +1,32 @@
+import React from 'react';
 import ReactDOM from 'react-dom';
+import ExampleWork from './example-work';
 
-console.log("Loaded react-dom");
+const myWork = [
+  {
+    'title':"Work Example 1",
+    'image': {
+        'desc': "Example of Kiosk EMD",
+        'src':"images/example1.png",
+        'comment':"This is comment 1"
+    }
+  },
+  {
+    'title':"Work Example 2",
+    'image': {
+        'desc': "Example of Agent Front End",
+        'src':"images/example2.png",
+        'comment':"This is comment 2"
+    }
+  },
+  {
+    'title':"Work Example 3",
+    'image': {
+        'desc': "Example of Serverless Portfolio",
+        'src':"images/example3.png",
+        'comment':"This is comment 3"
+    }
+  }
+]
 
-console.log("webpack works!nn");
+ReactDOM.render(<ExampleWork work={myWork}/>, document.getElementById('example-work'));
