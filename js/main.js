b03069f5bbae744a796332f3ee08b2acb554237e
Nicolas Spencer <nicolaspencer@gmail.com>
2018-01-16 22:45:32 -0300

    portfolio updates

--- js/main.js
+++ js/main.js
@@ -8,7 +8,7 @@ const myWork = [
     'href':"https://example.com",
     'desc':"Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
     'image': {
-        'desc': "Example of Kiosk EMD",
+        'desc': "Example of Kiosk EMDs",
         'src':"images/example1.png",
         'comment':"New Sales Model Kiosk EMD"
     }
