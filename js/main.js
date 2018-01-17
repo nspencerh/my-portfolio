efb5e096c78e60d48f9fe10f7bdf2929cdafd3ef
Nicolas Spencer <nicolaspencer@gmail.com>
2018-01-16 21:06:42 -0300

    commit updates to the portfolio

--- js/main.js
+++ js/main.js
@@ -4,13 +4,13 @@ import ExampleWork from './example-work';
 
 const myWork = [
   {
-    'title':"Work Example 1",
+    'title':"New Sales Model Kiosk EMD",
     'href':"https://example.com",
     'desc':"Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
     'image': {
         'desc': "Example of Kiosk EMD",
         'src':"images/example1.png",
-        'comment':"This is comment 1"
+        'comment':"New Sales Model Kiosk EMD"
     }
   },
   {
